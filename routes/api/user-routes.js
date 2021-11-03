9ed2f65bf3d0c2911554bd460d930ab07e272cce
Justin Bedford <jbedford94@gmail.com>
2021-11-02 21:30:44 -0400

    routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
@@ -18,7 +18,13 @@ router.get('/:id', (req, res) => {
     attributes: { exclude: ['password'] },
     where: {
       id: req.params.id
-    }
+    },
+    include: [
+      {
+        model: Post,
+        attributes: ['id', 'title', 'post_url', 'created_at']
+      }
+    ]
   })
     .then(dbUserData => {
       if (!dbUserData) {
@@ -73,20 +79,20 @@ router.post('/login', (req, res) => {
 router.put('/:id', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
-  // pass in req.body instead to only update what's passed through 
-  User.update(req.body, { 
-    individualHooks: true, 
-    where: { 
-      id: req.params.id 
-    } 
-  }) 
-    .then(dbUserData => { 
-      if (!dbUserData[0]) { 
-        res.status(404).json({ message: 'No user found with this id' }); 
-        return; 
-      } 
-      res.json(dbUserData); 
-    }) 
+  // pass in req.body instead to only update what's passed through
+  User.update(req.body, {
+    individualHooks: true,
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData[0]) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
